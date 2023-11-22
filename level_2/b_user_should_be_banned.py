8345ffba9c077c056c86f4f2d8da08cf7f5686ee
Eugene Myasnikov <justatrade@mail.ru>
2023-11-22 18:19:58 +0300

    OOP lesson 2 done

--- level_2/b_user_should_be_banned.py
+++ level_2/b_user_should_be_banned.py
@@ -11,4 +11,18 @@ SURNAMES_TO_BAN = ['Vaughn', 'Wilhelm', 'Santaros', 'Porter', 'Smith']
 
 
 class User:
-    pass  # код писать тут
+
+    def __init__(self, name: str, surname: str, age: int):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.is_banned = False
+        self.__should_be_banned()
+
+    def __should_be_banned(self):
+        if self.surname in SURNAMES_TO_BAN:
+            self.is_banned = True
+
+    def __str__(self):
+        return f'{self.name} {self.surname}, ' \
+               f'{self.age}' if not self.is_banned else 'Banned'
