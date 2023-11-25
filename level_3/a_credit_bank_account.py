8b6b3c5b1b4406678bc9fcf0d6ad3b4cad6ecf53
Eugene Myasnikov <justatrade@mail.ru>
2023-11-25 04:13:39 +0300

    OOP lesson 3 partly

--- level_3/a_credit_bank_account.py
+++ level_3/a_credit_bank_account.py
@@ -8,10 +8,8 @@
     4. Создать экземпляр класс CreditAccount и вызвать у него каждый из возможных методов.
 """
 
-# код писать тут
 
-
-class CreditAccount:
+class BankAccount:
     def __init__(self, owner_full_name: str, balance: float):
         self.owner_full_name = owner_full_name
         self.balance = balance
@@ -22,10 +20,27 @@ class CreditAccount:
     def decrease_balance(self, amount: float):
         self.balance -= amount
 
+
+class CreditAccount(BankAccount):
+    def __init__(self, owner_full_name: str, balance: float):
+        super().__init__(owner_full_name, balance)
+        self.owner_full_name = owner_full_name
+        self.balance = balance
+
     def is_eligible_for_credit(self):
         return self.balance > 1000
 
 
 if __name__ == '__main__':
-    pass  # код писать тут
-
+    ba = BankAccount('Ivan Ivanov', 123.45)
+    ca = CreditAccount('Petr Petrov', 1234.56)
+    print(ba.balance)
+    ba.increase_balance(100)
+    print(ba.balance)
+    ba.decrease_balance(10.5)
+    print(ba.balance)
+    print(ca.balance, ca.is_eligible_for_credit())
+    ca.increase_balance(500)
+    print(ca.balance, ca.is_eligible_for_credit())
+    ca.decrease_balance(1000)
+    print(ca.balance, ca.is_eligible_for_credit())
--- level_3/b_user_manage.py
+++ level_3/b_user_manage.py
@@ -22,9 +22,47 @@ class UserManager:
         return self.usernames
 
 
-# код писать тут
+class AdminManager(UserManager):
+    def __init__(self):
+        super().__init__()
+
+    def ban_username(self, username):
+        try:
+            self.usernames.remove(username)
+        except ValueError:
+            print('Такого пользователя не существует.')
+
+
+class SuperAdminManager(AdminManager):
+    def __init__(self):
+        super().__init__()
+
+    def ban_all_users(self):
+        self.usernames = []
 
 
 if __name__ == '__main__':
-    pass  # код писать тут
+    um = UserManager()
+    am = AdminManager()
+    sam = SuperAdminManager()
+    print(f'Users of UsersManager: {um.get_users()}')
+    um.add_user('Ivan')
+    um.add_user('Petr')
+    um.add_user('Alex')
+    print(f'Users of UsersManager: {um.get_users()}')
+
+    am.add_user('Ivan')
+    am.add_user('Petr')
+    am.add_user('Alex')
+    print(f'Users of AdminManager: {am.get_users()}')
+    am.ban_username('Ivan')
+    print()
+    print(f'Users of AdminManager: {am.get_users()}')
+    sam.add_user('Ivan')
+    sam.add_user('Petr')
+    sam.add_user('Alex')
+    print(f'Users of SuperAdminManager: {sam.get_users()}')
+    sam.ban_all_users()
+    print('Banned all')
+    print(f'Users of SuperAdminManager: {sam.get_users()}')
 
